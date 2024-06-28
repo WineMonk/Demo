328efc362ab179490370389869087a79595bab34
WineMonk <1361151272@qq.com>
2024-06-28 11:20:42 +0800

    initialize

--- CAD/TestCAD/Program.cs
+++ CAD/TestCAD/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            string dwgFile = "D:\\admin\\Desktop\\测试图-测绘&dfx&表格\\8东8支8北\\01-夏茅站结构总平面图.dwg";
+            string dwgFile = "data/test.dwg";
             CadDocReader cadDocReader = new CadDocReader(dwgFile);
             cadDocReader.ReadTable();
             Console.Read();
